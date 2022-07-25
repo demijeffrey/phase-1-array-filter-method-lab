830585c126a2fab418d6d99145280d476d8efd52
Demaris Ashmore <demiashmore@yahoo.com>
2022-07-25 14:39:56 -0600

    commit tree d373ac480f6c9c2b6b839692aef2414135ba1aad

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const findMatching = function (drivers, string) {
+    const names = drivers.filter(function (element) {
+        return element.toLowerCase() === string.toLowerCase()
+    })
+    return names
+}
+
+
+function fuzzyMatch (drivers, string) {
+    const letterNames = drivers.filter(element => {
+        return element[0] === string[0]
+        })
+        return letterNames
+}
+
+
+function matchName (drivers, string) {
+      return drivers.filter(obj => obj.name === string)
+}
